1cacd394ba20b2f574bd8bce64fa7ce675a04e81
zaphoxx <zaphoxx@gmail.com>
2017-07-11 00:00:53 +0200

    update

--- drivers/screen.h
+++ drivers/screen.h
@@ -8,20 +8,23 @@
 #define SCREEN_CTRL 0x3d4
 #define SCREEN_DATA 0x3d5
 
-void test(void);
-/** write_cell:
-* @param character
-* @param target address
-* @param attribute_byte
-**/
-void write_cell(unsigned char * frameBuffer,char character,unsigned int videoMem,unsigned char attribute_byte);
-/** printc:
-* print a single char to the screen
-* @param character - character to print
-* @param col - column
-* @param row - row
-* @param attribute_byte - foreground, background color)
-**/
-//void printc(unsigned char character,unsigned int col,unsigned int row,unsigned char attribute_byte);
+enum color{
+  black,
+  blue,
+  green,
+  cyan,
+  red,
+  magenta,
+  brown,
+  lightgrey,
+  darkgrey,
+  lightblue,
+  lightgreen,
+  lightcyan,
+  lightred,
+  lightmagenta,
+  lightbrown,
+  white
+};
 
 #endif
